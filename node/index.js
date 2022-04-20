8fa7db5c1371340b3bb8ce629d918bbc5042a60b
Cássio Izumi Krüger <cassio.kruger@selecty.com.br>
2022-04-20 08:17:51 -0300

    first commit

--- node/index.js
+++ node/index.js
@@ -10,14 +10,29 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
-connection.query(sql)
-connection.end()
+const create_table = `CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id))`
+connection.query(create_table)
 
+const insert = `INSERT INTO people(name) values('Cássio')`
+connection.query(insert)
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+connection.query('SELECT name FROM people', function(err, rows, fields){
+    if(err) throw err
+
+    let people = rows.map(function(e){
+        return e.name
+    }).join('<br>')
+
+    connection.end()
+
+    let title = '<h1>Full Cycle Rocks!</h1>'
+    let page = title+people
+
+    app.get('/', function(req, res) {
+      res.send(page)
+    })
 })
+
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
